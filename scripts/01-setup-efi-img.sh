8d53cbe60b679da910e802d95e0290b01cd829ca
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2023-01-08 14:13:04 +0100

    Fixed spelling mistake

--- scripts/01-setup-efi-img.sh
+++ scripts/01-setup-efi-img.sh
@@ -19,12 +19,13 @@ parted -s "${BASE_IMG_FILE}" mklabel gpt
 parted -s "${BASE_IMG_FILE}" mkpart primary fat32 1MiB 100%
 parted -s "${BASE_IMG_FILE}" set 1 esp on
 
+# Just for caution
 sleep 2
 
 # Create a loop device for the image file
 LOOP_DEV=$(losetup --find --show --partscan "${BASE_IMG_FILE}")
 
-# Fixes bug of /dev/loopp0p1 not found on slow systems
+# Fixes bug of /dev/loop0p1 not found by mkfs on slow systems
 sleep 5
 
 # Create a filesystem on the loop device
